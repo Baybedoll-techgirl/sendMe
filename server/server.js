72a8fed3aa5d791d88cc657822aedb617a9a8491
Tarita Jackson <ritajack@gmail.com>
2021-10-23 12:00:58 -0400

    fixes

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-// require("dotenv").config();
+require("dotenv").config();
 const stripe = require('stripe')(process.env.STRIPE_KEY)
 const express = require ('express')
 const mongoose = require('mongoose')
