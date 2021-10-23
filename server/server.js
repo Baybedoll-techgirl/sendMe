6421bc65c169eae69df3e64964ee1d3a646b6537
Tarita Jackson <ritajack@gmail.com>
2021-10-23 12:15:02 -0400

    fixes

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const path = require("path")
 
-
 const app = express();
 app.use(express.json());
 
@@ -42,10 +41,10 @@ app.post("/payment",  (req, res) => {
 if(process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"))
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  })
 }
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+})
 
 const port = process.env.PORT || 5000
 
